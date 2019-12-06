c8db06c256061d095fbc7fca8924b555a952b9c5
Milo Rastgoo <hello@informagician.com>
2019-12-06 12:36:49 -0500

    Cards completed

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,47 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardsContainer = document.querySelector('.cards-container');
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        let values = Object.values(response.data.articles);
+        console.log(values);
+        values.forEach(item => {
+            item.forEach(art => {
+                const newArticle = Article(art);
+                cardsContainer.appendChild(newArticle);
+            })
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    });
+function Article(data) {
+
+    const card = document.createElement('div'),
+        headline = document.createElement('div'),
+        author = document.createElement('div'),
+        imgContainer = document.createElement('div'),
+        img = document.createElement('img'),
+        by = document.createElement('span');
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    headline.textContent = data.headline;
+    img.setAttribute('src', data.authorPhoto);
+    by.textContent = `By ${data.authorName}`;
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    author.appendChild(by);
+    imgContainer.appendChild(img);
+
+    return card;
+}
+
